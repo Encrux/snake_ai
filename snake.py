d159f75c1a3f0dc49802cc4f25e39db5d14687fc
Daniel <boesch.daniel98@gmail.com>
2022-06-03 00:21:30 +0200

    clean up code

--- snake.py
+++ snake.py
@@ -1,81 +1,78 @@
 import pygame
-import time
 import random
-from enum import Enum
 import numpy as np
-import sys
-from gym import error, spaces, utils, core
+from gym import spaces
 import gym
-import game
 import math
 
 window_size = width, height = 1000, 1000
 grid_size = 20
 
 colors = {
-    "black": (0,0,0),
-    "white": (255,255,255),
-    "blue":  (0, 0, 255),
-    "red":   (255,0,0)
+    "black": (0, 0, 0),
+    "white": (255, 255, 255),
+    "blue": (0, 0, 255),
+    "red": (255, 0, 0)
 }
 
 dirs = [
-    [0,-1], [1,0], [0,1], [-1,0]
+    [0, -1], [1, 0], [0, 1], [-1, 0]
 ]
 
 default_snake = [
-            (25, 25),
-            (24, 25),
-            (23, 25),
-            (22, 25)
-        ]
+    (25, 25),
+    (24, 25),
+    (23, 25),
+    (22, 25)
+]
 
 
 def game_to_screen(x: int, y: int):
     return x * grid_size, y * grid_size
+
 
 def distance_reward(snake, apple):
     head = snake[0]
     distance = math.dist(head, apple)
     return ((1 / distance) - 0.5) * 10
 
-class snake_game(gym.Env):
+
+class SnakeEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     snake = []
     frame = []
 
-    def __init__(self, board_dim = (50, 50), simspeed = 500, initial_length = 4, initial_dir=[1,0]) -> None:
+    def __init__(self, board_dim=(50, 50), simspeed=500, initial_length=4, initial_dir=[1, 0]) -> None:
         self.dt = simspeed
         self.episode = 0
         self.cumulative_reward = 0
         self.steps = 0
         self.width, self.height = int(board_dim[0]), int(board_dim[1])
-        self.board_dim = board_dim 
+        self.board_dim = board_dim
 
         self.highscore = 0
 
         self.action_space = spaces.Discrete(4)
         self.observation_space = spaces.Box(low=0, high=2, shape=(board_dim))
 
-        #init game window
+        # init game window
         pygame.init()
         pygame.display.set_caption("snake:ai")
         self.window = pygame.display.set_mode(window_size)
         self.window.fill(colors["black"])
         pygame.display.flip()
-    
+
         self.reset()
 
-
     def reset(self):
-        if (len(self.snake) > self.highscore):
+        if len(self.snake) > self.highscore:
             self.highscore = len(self.snake)
             print("new highscore: ", self.highscore)
 
-        #print("score: " + str(len(self.snake)))
+        # print("score: " + str(len(self.snake)))
         self.score = 0
-        self.head_dir = 1 #right
+        self.head_dir = 1  # right
         self.snake = default_snake
         self.game_over = False
         self.spawn_apple()
@@ -86,9 +83,9 @@ class snake_game(gym.Env):
         spaces = list(np.ndindex(*self.board_dim))
         for block in self.snake:
             spaces.remove(block)
-        
-        random_index = random.randint(0, len(spaces) - 1)  
-        #print("apple: " + str(spaces[random_index]))
+
+        random_index = random.randint(0, len(spaces) - 1)
+        # print("apple: " + str(spaces[random_index]))
         self.apple = spaces[random_index]
 
     def is_valid(self):
@@ -99,21 +96,21 @@ class snake_game(gym.Env):
         head = self.snake[0]
         snake_copy.pop(0)
 
-        #check if head collides with rest
+        # check if head collides with rest
         for x, y in snake_copy:
             x_head, y_head = head
             if (x_head, y_head) == (x, y):
                 return False
 
-        #check map boundaries        
+        # check map boundaries
         x_max, y_max = self.board_dim
         x, y = head
 
         if x < 0 or x >= x_max:
             return False
-        if y < 0 or y >=     y_max:
+        if y < 0 or y >= y_max:
             return False
-        
+
         return True
 
     def valid_direction(self, action):
@@ -125,7 +122,7 @@ class snake_game(gym.Env):
             return False
         if self.head_dir == 3 and action == 1:
             return False
-        
+
         return True
 
     def step(self, action):
@@ -133,9 +130,9 @@ class snake_game(gym.Env):
         self.steps += 1
 
         if self.valid_direction(action):
-           self.head_dir = action 
+            self.head_dir = action
 
-        #update snake head
+            # update snake head
         head = tuple(np.add(self.snake[0], dirs[self.head_dir]))
         self.snake = [head] + self.snake
 
@@ -155,32 +152,33 @@ class snake_game(gym.Env):
 
         dr = distance_reward(self.snake, self.apple)
         reward += dr
-        #print(reward, dr)
+        # print(reward, dr)
         self.cumulative_reward += dr
 
-        return self.frame, reward, self.game_over, {'episode': {'episode': self.episode, 'r': self.cumulative_reward, 'l':self.steps}}
+        return self.frame, reward, self.game_over, {
+            'episode': {'episode': self.episode, 'r': self.cumulative_reward, 'l': self.steps}}
 
     def update_frame(self):
         next = np.zeros(shape=(self.width, self.height))
         next[self.apple] = 2
-        
+
         for block in self.snake:
             next[block] = 1
-        
+
         pygame.time.delay(self.dt)
         self.frame = next
-        #self.render()
+        # self.render()
 
     def render(self):
         self.window.fill(colors["black"])
 
-        #render snake
+        # render snake
         for block in self.snake:
             x, y = game_to_screen(*block)
             rect = pygame.Rect(x, y, grid_size, grid_size)
             pygame.draw.rect(self.window, colors["blue"], rect)
 
-        #render apple
+        # render apple
         ax, ay = game_to_screen(*self.apple)
         pygame.draw.rect(self.window, colors["red"], pygame.Rect(ax, ay, grid_size, grid_size))
         pygame.display.flip()
--- train_snake.py
+++ train_snake.py
@@ -2,21 +2,23 @@ import gym
 
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
-from snake import snake_game
+from snake import SnakeEnv
 
-def fromDefaultParameters():
-    return snake_game(board_dim=(50,50), simspeed=5, initial_length=4, initial_dir=[1,0])
+
+def from_default_parameters():
+    return SnakeEnv(board_dim=(50, 50), simspeed=5, initial_length=4, initial_dir=[1, 0])
+
 
 # Parallel environments
-snake_env = make_vec_env(fromDefaultParameters, n_envs=4)
+snake_env = make_vec_env(from_default_parameters, n_envs=24)
 
-model = PPO("MlpPolicy", snake_env, verbose=1)
+model = PPO("MlpPolicy", snake_env, verbose=1, device="auto")
 model.learn(total_timesteps=2500000)
-model.save("ppo_cartpole")
+model.save("smart_snek")
 
-del model # remove to demonstrate saving and loading
+del model  # remove to demonstrate saving and loading
 
-model = PPO.load("ppo_cartpole")
+model = PPO.load("smart_snek")
 
 obs = snake_env.reset()
 while True:
